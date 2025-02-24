8a701805f321e54d66925fb1ccf9322cb81b2444
Monchis <moniiikatty@gmail.com>
2025-02-23 22:12:21 -0500

    feat: add crud clientes

---
+++ 2_Clase/Models/ClienteModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Clase_2.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        public string Cedula_RUC { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public int? Edad { get; set; }
+        public bool Genero { get; set; }
+        public DateOnly? Fecha_Nacimiento { get; set; }
+    }
+}
