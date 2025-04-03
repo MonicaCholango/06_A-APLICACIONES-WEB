fe1b7e1b8ea4458d0bcf86759efaba4a10f675f7
Monchis <moniiikatty@gmail.com>
2025-04-02 20:06:16 -0500

    feat: add crud clientes

---
+++ Fase2/07_Tarea/Models/DetalleFacturaModel.cs
@@ -0,0 +1,29 @@
+﻿namespace _07_Tarea.Models
+{
+    public class DetalleFacturaModel
+    {
+        public int Id { get; set; }
+        public int Cantidad { get; set; }
+        public float valor { get; set; }
+
+        ///relaciones 
+        ///Productos
+        public int ProductoModelsId { get; set; }
+        public ProductoModels ProductoModels { get; set; }
+
+
+        //factura  -- cabecera de la factura
+        public int FacturaModelId { get; set; }
+        public FacturaModel FacturaModel { get; set; }
+
+        //relacion con stock
+
+        public int StockModelsId { get; set; }
+        public StockModels StockModels { get; set; }
+
+        //usuario
+        public string IdentityUserId { get; set; }
+        public UsuariosModel UsuariosModel { get; set; }
+
+    }
+}
---
+++ Fase2/07_Tarea/Models/UsuariosModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+//UUID
+using System.ComponentModel.DataAnnotations;
+
+namespace _07_Tarea.Models
+{
+    public class UsuariosModel : IdentityUser
+    {
+        [Required]
+        public string Cedula { get; set; }
+    }
+}
